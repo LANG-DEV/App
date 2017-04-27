afb48848035545e01d57c09ca4cef9f4fe87d156
ABigBigWhale <chakalakafrank@gmail.com>
2017-04-26 21:23:14 -0700

    Added file directory and start file for Home and Login

--- app/index.js
+++ app/index.js
@@ -1,10 +1,62 @@
 /**
- * Entry point for the App
- *
+ * Sample React Native App
+ * https://github.com/facebook/react-native
  * @flow
  */
 
 import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native@
+        </Text>
+        <Text style={styles.instructions}>
+          This is made by Mengmeng and Hao
+        </Text>
+        <Text style={styles.instructions}>
+          "To get started, edit index.ios.js"
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
 
-// TODO: everything
+const AppNavigation = StackNavigator({
+    Home: {Screen: HomeScreen},
+    Login: {Screen: LoginScreen}
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('App', () => App);
