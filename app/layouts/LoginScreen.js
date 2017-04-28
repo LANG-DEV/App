bf3c97af84dfcd56422aa47b2703d0eec3f2be43
ABigBigWhale <chakalakafrank@gmail.com>
2017-04-27 21:19:00 -0700

    Resolved merge conflict

--- app/layouts/LoginScreen.js
+++ app/layouts/LoginScreen.js
@@ -11,20 +11,20 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import auth from '../lib/auth';
 
 export default class LoginScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Login',
-  };
-
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-        username: '',
-        password: '',
+    static navigationOptions = {
+        title: 'Login',
     };
 
-    this.imageHeight = new Animated.Value(200);
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+        };
+
+        this.imageHeight = new Animated.Value(200);
+    }
 
     attemptLogin = (res) => {
         console.log("res: " + JSON.stringify(res));
@@ -51,123 +51,129 @@ export default class LoginScreen extends React.Component {
     onLoginButtonPressed = () => {
         auth.login(this.state.username, this.state.password, this.attemptLogin);
     }
-    
+
     componentWillMount () {
-      this.keyboardWillShowSub = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
-      this.keyboardWillHideSub = Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
+        this.keyboardWillShowSub = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
+        this.keyboardWillHideSub = Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
     }
 
     componentDidMount() {
         MessageBarManager.registerMessageBar(this.refs.alert);
     }
 
-  componentWillUnmount() {
-    this.keyboardWillShowSub.remove();
-    this.keyboardWillHideSub.remove();
-    
-    MessageBarManager.unregisterMessageBar();
-  }
+    componentWillUnmount() {
+        this.keyboardWillShowSub.remove();
+        this.keyboardWillHideSub.remove();
 
-  keyboardWillShow = (event) => {
-    Animated.timing(this.imageHeight, {
-      duration: event.duration,
-      toValue: 50,
-    }).start();
-  };
+        MessageBarManager.unregisterMessageBar();
+    }
 
-  keyboardWillHide = (event) => {
-    Animated.timing(this.imageHeight, {
-      duration: event.duration,
-      toValue: 200,
-    }).start();
-  };
+    keyboardWillShow = (event) => {
+        Animated.timing(this.imageHeight, {
+            duration: event.duration,
+            toValue: 50,
+        }).start();
+    };
 
-  render() {
-    return (
-      <KeyboardAvoidingView
-        behavior="padding"
-        style={styles.container}>
+    keyboardWillHide = (event) => {
+        Animated.timing(this.imageHeight, {
+            duration: event.duration,
+            toValue: 200,
+        }).start();
+    };
 
-        <View style={styles.logoContainer}>
-          <Animated.Image
-            style={[styles.logo, { height: this.imageHeight }]}
-            source={require('../images/logo_placeholder.png')} />
-          <Text style={styles.title}>
-            浪！
-          </Text>
-        </View>
+    render() {
+        return (
+            <KeyboardAvoidingView
+                behavior="padding"
+                style={styles.container}>
 
-        <View style={styles.formContainer}>
-          <TextInput
-            returnKeyType="next"
-            style={styles.input}
-            placeholder="Enter Username"
-            onChangeText={(text) => this.setState({username: text})}
-            onSubmitEditing={() => this.passwordInput.focus()}/>
-          <TextInput
-            ref={(input) => this.passwordInput = input}
-            returnKeyType="go"
-            style={styles.input}
-            placeholder="Enter Password"
-            secureTextEntry={true}
-            onChangeText={(text) => this.setState({password: text})}/>
-          <TouchableOpacity style={styles.buttonContainer}>
-            <Button title="LOGIN" style={styles.buttonText} />
-          </TouchableOpacity>
-        </View>
+                <MessageBar ref='alert' />
 
-      </KeyboardAvoidingView>
-    )
-  }
-}
+                <View style={styles.logoContainer}>
+                    <Animated.Image
+                        style={[styles.logo, { height: this.imageHeight }]}
+                        source={require('../images/logo_placeholder.png')} />
+                    <Text style={styles.title}>
+                        浪！
+                    </Text>
+                </View>
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#3498db',
-    flex: 1,
-    padding: 20,
-    justifyContent: 'space-between'
-  },
-  buttonContainer: {
-    backgroundColor: '#2980b9',
-    paddingVertical: 10
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: '#FFF'
-  },
-  logo: {
-    height: 200,
-    width: 200,
-    //flex: 1,
-    resizeMode: 'contain'
-  },
-  logoContainer: {
-    alignItems: 'center',
-    flex: 1,
-    flexGrow: 1,
-    justifyContent: 'center',
-    //resizeMode: 'cover'
-  },
-  formContainer: {
-    justifyContent: 'flex-end',
-    padding: 20,
-    //paddingBottom: 40
-    //margin: 30
-  },
-  input: {
-    backgroundColor: 'rgba(255,255,255,0.2)',
-    color: '#FFF',
-    height: 40,
-    padding: 10,
-    marginVertical: 10,
-  },
-  title: {
-    color: '#FFF',
-    fontWeight: 'bold',
-    fontSize: 40,
-    marginTop: 10,
-    marginLeft: 30,
-    textAlign: 'center'
-  }
-});
+                <View style={styles.formContainer}>
+                    <TextInput
+                        returnKeyType="next"
+                        style={styles.input}
+                        placeholder="Enter Username"
+                        onChangeText={(text) => this.setState({username: text})}
+                        onSubmitEditing={() => this.passwordInput.focus()}/>
+                    <TextInput
+                        ref={(input) => this.passwordInput = input}
+                        returnKeyType="go"
+                        style={styles.input}
+                        placeholder="Enter Password"
+                        secureTextEntry={true}
+                        onChangeText={(text) => this.setState({password: text})}/>
+                    <TouchableOpacity style={styles.buttonContainer}>
+                        <Button
+                            title="LOGIN"
+                            style={styles.buttonText}
+                            onPress={this.onLoginButtonPressed}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+            </KeyboardAvoidingView>
+                    )
+                }
+            }
+
+            const styles = StyleSheet.create({
+                container: {
+                    backgroundColor: '#3498db',
+                    flex: 1,
+                    padding: 20,
+                    justifyContent: 'space-between'
+                },
+                buttonContainer: {
+                    backgroundColor: '#2980b9',
+                    paddingVertical: 10
+                },
+                buttonText: {
+                    textAlign: 'center',
+                    color: '#FFF'
+                },
+                logo: {
+                    height: 200,
+                    width: 200,
+                    //flex: 1,
+                    resizeMode: 'contain'
+                },
+                logoContainer: {
+                    alignItems: 'center',
+                    flex: 1,
+                    flexGrow: 1,
+                    justifyContent: 'center',
+                    //resizeMode: 'cover'
+                },
+                formContainer: {
+                    justifyContent: 'flex-end',
+                    padding: 20,
+                    //paddingBottom: 40
+                    //margin: 30
+                },
+                input: {
+                    backgroundColor: 'rgba(255,255,255,0.2)',
+                    color: '#FFF',
+                    height: 40,
+                    padding: 10,
+                    marginVertical: 10,
+                },
+                title: {
+                    color: '#FFF',
+                    fontWeight: 'bold',
+                    fontSize: 40,
+                    marginTop: 10,
+                    marginLeft: 30,
+                    textAlign: 'center'
+                }
+            });
